4c07bf74343ffed0ff4cd06a71c77286b26ac588
Mattan Dor <mattan.dor1@gmail.com>
2025-08-17 16:58:55 +0300

    Added

--- classes/gateway_client.php
+++ classes/gateway_client.php
@@ -173,7 +173,7 @@ class gateway_client {
               'User-Agent: Moodle TrustGrade Plugin'
           ],
           CURLOPT_TIMEOUT => $this->timeout,
-          CURLOPT_SSL_VERIFYPEER => false,
+          CURLOPT_SSL_VERIFYPEER => true,
           CURLOPT_FOLLOWLOCATION => true,
           CURLOPT_MAXREDIRS => 3
       ]);
