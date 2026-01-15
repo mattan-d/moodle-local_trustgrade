41f2b5dcd69e5ca0e271b507e62df75bbb10b61c
Mattan Dor <mattan.dor1@gmail.com>
2026-01-15 23:17:45 +0200

    feat: add memory and time limits for adhoc task execution

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_trustgrade';
-$plugin->version = 2026010201;
+$plugin->version = 2026011500;
 $plugin->requires = 2022112800; // Moodle 4.1
 $plugin->maturity = MATURITY_STABLE;
-$plugin->release = '1.6.0';
+$plugin->release = '1.7.0';
