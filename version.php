36f4a5d99f7e7f061eefd685c288d641c5fe1dc8
Mattan Dor <mattan.dor1@gmail.com>
2026-02-12 10:54:03 +0200

    chore: update version number to 2026021200 in version.php for local_trustgrade

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_trustgrade';
-$plugin->version = 2026021001;
+$plugin->version = 2026021200;
 $plugin->requires = 2022112800; // Moodle 4.1
 $plugin->maturity = MATURITY_STABLE;
 $plugin->release = '3.0';
