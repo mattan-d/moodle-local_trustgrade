303f3a0ddc21303d95e4015d625c59e4e5d10905
Mattan Dor <mattan.dor1@gmail.com>
2026-03-26 13:56:33 +0200

    chore: update version number and release information for local_trustgrade plugin

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_trustgrade';
-$plugin->version = 2026032400;
+$plugin->version = 2026032600;
 $plugin->requires = 2022112800; // Moodle 4.1
 $plugin->maturity = MATURITY_STABLE;
-$plugin->release = '3.5';
+$plugin->release = '3.6';
